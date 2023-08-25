af27017bdb5e0f1be31cea8af6c669dd3b1854f4
Mathias Heyraud <heyraud.mathias@gmail.com>
2023-08-25 22:36:32 +0200

    Création fichier home.html et BDD liste_des_taches

--- liste_des_taches/gestion_taches/admin.py
+++ liste_des_taches/gestion_taches/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from gestion_taches.models import liste_taches
 
+class ListeTachesAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'auteur')
+
+admin.site.register(liste_taches, ListeTachesAdmin)
 # Register your models here.
--- liste_des_taches/gestion_taches/models.py
+++ liste_des_taches/gestion_taches/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class liste_taches(models.Model):
+
+    titre = models.fields.CharField(max_length=100)
+    auteur = models.fields.CharField(max_length=50)
+    active = models.fields.BooleanField(default=True)
+    description = models.fields.CharField(max_length=500)
+    date_creation = models.fields.DateTimeField(
+        verbose_name="Date et heure de création de la tâche"
+        )
+    date_cloture = models.fields.DateTimeField(
+        verbose_name='Date et heure de cloture de la tâche',
+        default=None
+    )
+    
